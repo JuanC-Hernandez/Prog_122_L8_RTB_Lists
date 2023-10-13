546c6273aa93a6652372c6c760c8e431874e6d26
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2023-10-12 19:59:59 -0700

    Note Update 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,47 @@ namespace Prog_122_L8_RTB_Lists
         public MainWindow()
         {
             InitializeComponent();
+            ArrayDemonstration();
+        }//MainWindow
+        public void ArrayDemonstration()
+        {
+            string[] students = new string[4];
+            students[0] = "Hafsa";
+            students[1] = "Kira";
+            students[2] = "Juan";
+            students[3] = "Matt";
+
+            //rtbDisplay.Text = students[0];
+            for (int i = 0; i<students.Length; i++)
+            {
+                rtbDisplay.Text += $"{students[i]}\n";
+            }
         }
-    }
-}
+        private void btnDisplayText_Click(object sender, RoutedEventArgs e)
+        {
+            AccidentInfo();
+
+        }// btnDisplayText_Click    
+
+        public void AccidentInfo()
+        {
+            string driver = txtDriverName.Text;
+            string address = txtAddress.Text;
+            string make = txtMake.Text; 
+            string model = txtModel.Text;
+            string year = txtYear.Text;
+            string info = rtbAccidentInfo.Text;
+
+            //string formatedInformation = $"{make}{model}{year}{info}{driver}{address}";
+
+            rtbDisplay.Text = $"Driver Name: {driver}\n";
+            rtbDisplay.Text += $"Address: {address}\n";
+            rtbDisplay.Text += $"Make: {make}\n";
+            rtbDisplay.Text += $"Model: {model}\n";
+            rtbDisplay.Text += $"Year: {year}\n";
+            rtbDisplay.Text += $"Info: {info}\n";
+        }//Accident Info
+
+
+    }//class
+}//namespace
